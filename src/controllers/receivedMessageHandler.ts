53b9ec8288bc5c144a72214d3039b7edcc3acb3b
Ata Türkoğlu <ata.turkoglu@reengen.com>
2024-10-20 22:46:33 +0300

    add broadcasts

--- src/controllers/receivedMessageHandler.ts
+++ src/controllers/receivedMessageHandler.ts
@@ -1,12 +1,16 @@
 import WebSocket from "ws";
 import { IdTagInfo, SocketInfo } from "../models/common";
 import { AuthorizeReq } from "../models/requests";
-import { BootNotificationRes, StartTransactionRes } from "../models/responses";
+import {
+    BootNotificationRes,
+    StartTransactionRes,
+    StopTransactionRes,
+} from "../models/responses";
 import SessionManager from "../services/SessionManager";
 import Authorization from "../services/Authorization";
 import MessageSendingHandler from "../services/MessageSendingHandler";
 import BroadcastService from "../services/BroadcastService";
-import { convertKeysForDB } from "../utils";
+import { convertKeysForDB, getUserName } from "../utils";
 
 const ReceivedMessageHandler = (
     socket: WebSocket & SocketInfo,
@@ -32,7 +36,13 @@ const ReceivedMessageHandler = (
                 clients
             );
         case "StartTransaction":
-            return startTransactionMsg(socket, uniqueId, payload, charger);
+            return startTransactionMsg(
+                socket,
+                uniqueId,
+                payload,
+                charger,
+                clients
+            );
         case "StatusNotification":
             return statusNotificationMsg(
                 socket,
@@ -42,9 +52,15 @@ const ReceivedMessageHandler = (
                 clients
             );
         case "MeterValues":
-            return meterValuesMsg(socket, uniqueId, payload, charger);
+            return meterValuesMsg(socket, uniqueId, payload, charger, clients);
         case "StopTransaction":
-            return stopTransactionMsg(socket, uniqueId, payload, charger);
+            return stopTransactionMsg(
+                socket,
+                uniqueId,
+                payload,
+                charger,
+                clients
+            );
         case "Heartbeat":
             return heartbeatMsg(socket, uniqueId, payload, charger);
         default:
@@ -88,9 +104,10 @@ const startTransactionMsg = async (
     socket: WebSocket & SocketInfo,
     uniqueId: string,
     data: any,
-    charger: string
+    charger: string,
+    clients: any
 ) => {
-    const response = await SessionManager(
+    const response: any = await SessionManager(
         socket.idTag,
         "startTransaction",
         data,
@@ -98,6 +115,27 @@ const startTransactionMsg = async (
     );
 
     MessageSendingHandler(socket, 3, uniqueId, response);
+
+    if (response.idTagInfo.status == "Accepted") {
+        const userName = await getUserName(data.idTag);
+        const newData = {
+            userName,
+            ...data,
+            start_timestamp: data.timestamp,
+        };
+        delete newData.timestamp;
+        const broadcastData = {
+            ...newData,
+            transactionId: response.transactionId,
+        };
+
+        BroadcastService(
+            "startTransaction",
+            charger,
+            clients,
+            convertKeysForDB(broadcastData, false)
+        );
+    }
 };
 
 const statusNotificationMsg = async (
@@ -128,7 +166,8 @@ const meterValuesMsg = async (
     socket: WebSocket & SocketInfo,
     uniqueId: string,
     data: any,
-    charger: string
+    charger: string,
+    clients: any
 ) => {
     const response = await SessionManager(
         socket.idTag,
@@ -138,15 +177,23 @@ const meterValuesMsg = async (
     );
 
     MessageSendingHandler(socket, 3, uniqueId, response);
+
+    BroadcastService(
+        "meterValues",
+        charger,
+        clients,
+        convertKeysForDB(data, false)
+    );
 };
 
 const stopTransactionMsg = async (
     socket: WebSocket & SocketInfo,
     uniqueId: string,
     data: any,
-    charger: string
+    charger: string,
+    clients: any
 ) => {
-    const response = await SessionManager(
+    const response: any = await SessionManager(
         socket.idTag,
         "stopTransaction",
         data,
@@ -154,6 +201,23 @@ const stopTransactionMsg = async (
     );
 
     MessageSendingHandler(socket, 3, uniqueId, response);
+
+    if (response.idTagInfo.status == "Accepted") {
+        const broadcastData = {
+            ...data,
+            endTimestamp: data.timestamp,
+            meterValues: data.transactionData,
+        };
+        delete broadcastData.timestamp;
+        delete broadcastData.transactionData;
+
+        BroadcastService(
+            "stopTransaction",
+            charger,
+            clients,
+            convertKeysForDB(broadcastData, false)
+        );
+    }
 };
 
 const heartbeatMsg = (
